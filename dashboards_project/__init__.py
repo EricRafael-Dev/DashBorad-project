c884bcb310d1a76df3ebd7565dc929d2658d54c6
Eric Rafael <rafaelpsec@gmail.com>
2024-11-25 13:41:43 -0300

    converting dates and organizing months

--- dashboards_project/__init__.py
+++ dashboards_project/__init__.py
@@ -8,4 +8,13 @@ st.set_page_config(layout="wide")
 
 #loading files to read
 df = pd.read_csv("D:\EricHDD\Projects\dashboards_project\supermarket_sales.csv", sep=";", decimal=",")
-df
+
+#converting "date" to library understand and organize
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values("Date")
+
+df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
+
+month = st.sidebar.selectbox("Month", df["Month"].unique())
+
+filter = df[df["Month"] == month]
