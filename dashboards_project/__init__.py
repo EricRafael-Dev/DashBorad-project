21c657a732e5c75e3252616c75b6f62fd8d5ff1d
Eric Rafael <rafaelpsec@gmail.com>
2024-11-26 11:27:59 -0300

    filter dates, create graps and showing them

--- dashboards_project/__init__.py
+++ dashboards_project/__init__.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import pandas as pd
 import streamlit as st
+import nbformat
 
 #config AppWeb layout
 
@@ -11,10 +12,32 @@ df = pd.read_csv("D:\EricHDD\Projects\dashboards_project\supermarket_sales.csv",
 
 #converting "date" to library understand and organize
 df["Date"] = pd.to_datetime(df["Date"])
+df["Quantity"] = pd.to_numeric(df["Quantity"])
+
 df = df.sort_values("Date")
 
 df["Month"] = df["Date"].apply(lambda x: str(x.year) + "-" + str(x.month))
 
 month = st.sidebar.selectbox("Month", df["Month"].unique())
 
+
+#filtering data per month for time range graphs
 filter = df[df["Month"] == month]
+
+#create graps
+fig_billing = px.bar(filter, x='Date', y='Total', color="City", title="Billing per year")
+
+city_total = filter.groupby("City")["Total"].sum().reset_index() #grouping Cities with summing the total
+fig_cities = px.bar(city_total, x='City', y='Total', title="Billing per city")
+
+fig_payments = px.pie(filter, values="Total", names="Payment", title="Payment Forms")
+
+rating_mean = filter.groupby("City")["Rating"].mean().reset_index() #calculing mean ranting and grouping with cities
+fig_rating = px.bar(rating_mean, y='Rating', x='City', title="Rating per city")
+
+
+#showing graps
+fig_billing
+fig_cities
+fig_payments
+fig_rating
